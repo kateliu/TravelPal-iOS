026cb90e81175bd993dc6b12e77e83b023b8146f
Kate Liu <kateycliu@gmail.com>
2013-09-22 09:21:49 -0700

    refine current travel view

--- TravelPal/TPCurrentTravelViewController.h
+++ TravelPal/TPCurrentTravelViewController.h
@@ -14,14 +14,11 @@
 }
 
 @property (nonatomic, strong) NSString *travelId;
-
 @property (nonatomic, strong) NSString *travelName;
-
 @property (nonatomic, strong) NSArray *events;
-
 @property (nonatomic, strong) UITableView *eventsTable;
-
 @property (nonatomic, strong) UILabel *travelNameLabel;
+@property (weak, nonatomic) IBOutlet UIButton *endTripButton;
 
 - (IBAction)endTravel:(id)sender;
 
